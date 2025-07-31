2545433ecb96b7305bed15d54609b0460ce76af7
kero-784 <kirlosayman141@gmail.com>
2025-07-31 20:11:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -393,7 +393,7 @@ function getText(key, ...args) {
 
 document.addEventListener('DOMContentLoaded', () => {
     // !!! IMPORTANT: PASTE YOUR GOOGLE APPS SCRIPT WEB APP URL HERE
-    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw6ILWFJPd8gsFQY-h4ZVZSByQfSXIzl6OpKk2m488Ihu9u1TCFSxsWAjvkW5Ws65NU/exec';
+    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyb8nL08KgxXTIHFCLhhe4Mu06UEk8dILG4Cbzda9kXhBCDkHbmeyZqD237ImdqvdiF/exec';
 
     const Logger = {
         info: (message, ...args) => console.log(`[StockWise INFO] ${message}`, ...args),
